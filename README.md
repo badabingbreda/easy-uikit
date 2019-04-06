d263d2b7968da68e3c91edaab54b70252fd03cd0
Didou Schol <didou@didou.nl>
2019-04-06 10:26:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+===Easy UIkit===
+Contributors: badabingbreda
+Donate link:
+Tags: framework, uikit, grid, flex, beaver builder, beaver themer, toolbox, cftoolbox
+Requires at least: 4.9.6
+Tested up to: 5.1.1
+Requires PHP: 5.6.3
+Stable tag: 4.3
+License: MIT
+License URI: [https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT)
+
+===Description===
+
+Easy UIKit adds the UIkit library (v3) to the queue so you can use it even when changing themes. Great for page builders plugins like Beaver Builder. It also includes AlekVolsk's width-ex for extended column support (up to 30 columns).
+
+More info on UIKit here: [https://getuikit.com](https://getuikit.com/docs/introduction)
---
+++ easy-uikit.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ Plugin Name: Easy UIkit
+ Plugin URI: https://www.beaverplugins.com/docs/easy-uikit/
+ Description: Easy UIKit adds the UIkit library (v3) to the queue so you can use it even when changing themes.
+ Version: 1.1.1
+ Author: Didou Schol
+ Text Domain: easy-uikit
+ Domain Path: /languages
+ Author URI: https://www.badabing.nl
+ */
+
+define( 'EASYUIKIT_VERSION' 	, '1.1.1' );
+define( 'EASYUIKIT_DIR'			, plugin_dir_path( __FILE__ ) );
+define( 'EASYUIKIT_FILE'		, __FILE__ );
+define( 'EASYUIKIT_URL' 		, plugins_url( '/', __FILE__ ) );
+
+add_action( 'wp_enqueue_scripts', 'easy_uikit_scripts_styles', 99, 1 );
+
+/**
+ * Enqueue script(s) and style(s)
+ *
+ */
+function easy_uikit_scripts_styles() {
+	wp_enqueue_script( 'easy-uikitjs', EASYUIKIT_URL.'/js/uikit.min.js', false, EASYUIKIT_VERSION , false );
+	wp_enqueue_script( 'easy-uikiticons', EASYUIKIT_URL.'/js/uikit-icons.min.js', false, EASYUIKIT_VERSION , false );
+	wp_enqueue_style( 'easy-uikitcss', EASYUIKIT_URL.'/css/bbuikit.theme.min.css', false, EASYUIKIT_VERSION , 'all' );
+}
+
+/**
+ * Load the Github Updater
+ */
+if( ! class_exists( 'github_updater' ) ){
+	include_once( EASYUIKIT_DIR . '/classes/class-github-updater.php' );
+}
+
+$updater = new github_updater( EASYUIKIT_FILE );
+$updater->set_username( 'badabingbreda' );
+$updater->set_repository( 'easy-uikit' );
+$updater->set_settings( array(
+			'requires'			=> '4.6',
+			'tested'			=> '5.1.1',
+			'rating'			=> '100.0',
+			'num_ratings'		=> '10',
+			'downloaded'		=> '10',
+			'added'				=> '2019-04-06',
+		) );
+$updater->initialize();
